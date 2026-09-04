e0294fd89e9ccd58c318c8ba8d9a24e5c161b721
Felipe Amelong <felipeamelong@gmail.com>
2026-09-04 18:18:06 -0300

    Métodos

--- sistema-de-alumnos/Program.cs
+++ sistema-de-alumnos/Program.cs
@@ -3,12 +3,6 @@
 Alumno alumno1 = new Alumno("Tomás", 1, 9, 7);
 Alumno alumno2 = new Alumno("Facundo", 2, 8.53m, 9.02m);
 
-Console.WriteLine($"Alumno 1:\nNombre: {alumno1.Nombre}\nLegajo: {alumno1.Legajo}");
+Console.WriteLine($"Alumno 1:\nNombre: {alumno1.Nombre}\nLegajo: {alumno1.Legajo}\nPromedio: {alumno1.Promedio()}");
 Console.WriteLine();
-Console.WriteLine($"Alumno 2:\nNombre: {alumno2.Nombre}\nLegajo: {alumno2.Legajo}");
-
-alumno1.Nombre = "Juan";
-
-Console.WriteLine($"Alumno 1:\nNombre: {alumno1.Nombre}\nLegajo: {alumno1.Legajo}");
-Console.WriteLine();
-Console.WriteLine($"Alumno 2:\nNombre: {alumno2.Nombre}\nLegajo: {alumno2.Legajo}");
+Console.WriteLine($"Alumno 2:\nNombre: {alumno2.Nombre}\nLegajo: {alumno2.Legajo}\nPromedio: {alumno2.Promedio()}");
