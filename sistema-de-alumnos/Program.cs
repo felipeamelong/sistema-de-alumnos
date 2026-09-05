7715cd4c1437f18c513b346ec6fcc63bd1926f08
Felipe Amelong <felipeamelong@gmail.com>
2026-09-05 16:24:22 -0300

    Encapsulamiento

--- sistema-de-alumnos/Program.cs
+++ sistema-de-alumnos/Program.cs
@@ -1,7 +1,18 @@
 ﻿using sistema_de_alumnos;
 
-Alumno alumno1 = new Alumno("Tomás", 1, 9, 7);
-Alumno alumno2 = new Alumno("Facundo", 2, 8.53m, 9.02m);
+Alumno alumno1 = new Alumno("Tomás", 1);
+Alumno alumno2 = new Alumno("Facundo", 2);
+
+bool resultado1 = alumno1.CargarNotas(8, 9);
+if (!resultado1)
+{
+    Console.WriteLine("Notas inválidas. No se cargaron en el sistema");
+}
+bool resultado2 = alumno2.CargarNotas(8.54, 5);
+if (!resultado2)
+{
+    Console.WriteLine("Notas inválidas. No se cargaron en el sistema");
+}
 
 Console.WriteLine(alumno1);
 Console.WriteLine(alumno2);
